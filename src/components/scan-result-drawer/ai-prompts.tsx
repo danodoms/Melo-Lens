e84d7b012f5e8676e622f863b0363b66a2f0ea79
danodoms <danodoms@gmail.com>
2025-03-18 10:15:43 +0800

    results drawer now has separate page

--- src/components/scan-result-drawer/ai-prompts.tsx
+++ src/components/scan-result-drawer/ai-prompts.tsx
@@ -5,10 +5,13 @@ import { Text } from "@/src/components/ui/text";
 import { VStack } from "@/src/components/ui/vstack";
 import { HStack } from "@/src/components/ui/hstack";
 import { getCompletion } from "../../lib/ai/fetch";
+import { AiSession } from "./index";
 
 
 
-export const RenderAiPrompts = ({ drawerState, setAiResponse }: { drawerState: any, setAiResponse: (response: string) => void }) => {
+
+
+export const RenderAiPrompts = ({ drawerState, setAiSession }: { drawerState: any, setAiSession: (aiSession: AiSession) => void }) => {
 
     const prompts = [
         "What are the possible treatments?",
@@ -18,12 +21,12 @@ export const RenderAiPrompts = ({ drawerState, setAiResponse }: { drawerState: a
 
     const generatePrompt = (prompt: string) => {
         const promptPrefix = "In a concise manner,";
-        return `${promptPrefix} ${prompt} for ${drawerState.classification}`;
+        return `${promptPrefix} ${prompt} for watermelon ${drawerState.classification}`;
     }
 
     const handleAiPrompt = async (prompt: string) => {
         const response = await getCompletion(prompt);
-        setAiResponse(response);
+        setAiSession({ prompt, response });
     };
 
 
@@ -32,7 +35,7 @@ export const RenderAiPrompts = ({ drawerState, setAiResponse }: { drawerState: a
         <VStack className="w-full mt-8">
             <HStack className="gap-2 items-center opacity-50">
                 <Bot color="white" className="size-sm" />
-                <Text className="font-bold">AI Responses</Text>
+                <Text className="font-bold">Ask AI</Text>
             </HStack>
 
             {prompts.map((prompt, index) =>
--- src/components/scan-result-drawer/index.tsx
+++ src/components/scan-result-drawer/index.tsx
@@ -3,7 +3,7 @@ import { Pressable, StyleSheet } from "react-native";
 import LottieView from "lottie-react-native";
 import Markdown from "react-native-markdown-display";
 import { Box } from "@/src/components/ui/box";
-import { Button, ButtonText } from "@/src/components/ui/button";
+import { Button, ButtonIcon, ButtonText } from "@/src/components/ui/button";
 import { Center } from "@/src/components/ui/center";
 import { Drawer, DrawerBackdrop, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader } from "@/src/components/ui/drawer";
 import { Heading } from "@/src/components/ui/heading";
@@ -14,6 +14,7 @@ import { VStack } from "@/src/components/ui/vstack";
 import { RenderAiPrompts } from "./ai-prompts";
 import { renderSaveResultComponent } from "./save-result-button";
 import { renderConfidenceRemark } from "./confidence-remark";
+import { MoveLeft } from "lucide-react-native";
 
 export type DrawerState = {
     isDrawerOpen: boolean;
@@ -31,10 +32,19 @@ type ScanResultDrawerProps = {
     drawerState: DrawerState;
 }
 
+export type AiSession = {
+    prompt: string;
+    response: string;
+}
+
 const ScanResultDrawer: React.FC<ScanResultDrawerProps> = ({ drawerState }) => {
-    const [aiResponse, setAiResponse] = useState<string>("test");
+    const [aiSession, setAiSession] = useState<AiSession>({ prompt: "", response: "" });
+    const [isAiPageShown, setIsAiPageShown] = useState(false);
+
     const [isXaiHeatmapShown, setIsXaiHeatmapShown] = useState(false);
     const isPredictionDone = drawerState.classification && drawerState.confidence && drawerState.imageUri;
+    const canSaveResult = drawerState.confidence && drawerState.classification && drawerState.imageUri;
+    const hasResults = drawerState.classification && drawerState.confidence;
 
     function handleSetXaiHeatmapShown() {
         if (drawerState.xaiHeatmapUri) {
@@ -51,68 +61,111 @@ const ScanResultDrawer: React.FC<ScanResultDrawerProps> = ({ drawerState }) => {
         >
             <DrawerBackdrop />
             <DrawerContent>
-                <DrawerHeader>
-                    {drawerState.classification && drawerState.confidence ? (
-                        <VStack>
-                            <Heading size="xl" className="text-left">
-                                {drawerState.classification}
-                            </Heading>
-                            <Text className="text-typography-400">
-                                {drawerState.confidence}% {renderConfidenceRemark(drawerState.confidence)} Confidence
+
+                {/* Render AI Response Page if it's shown */}
+                {isAiPageShown ? (
+                    <>
+                        <DrawerHeader>
+                            <VStack>
+                                <Heading size="xl" className="text-end">
+                                    {drawerState.classification}
+                                </Heading>
+                                <Text className="text-typography-400">
+                                    {drawerState.confidence}% {renderConfidenceRemark(drawerState.confidence ?? 0)} Confidence
+                                </Text>
+                            </VStack>
+
+                            <Button onPress={() => setIsAiPageShown(false)} className="">
+                                <ButtonIcon as={MoveLeft} />
+                            </Button>
+                        </DrawerHeader>
+                        <DrawerBody>
+                            <Text>
+                                {aiSession.prompt}
                             </Text>
-                        </VStack>
-                    ) : (
-                        <SkeletonText _lines={2} speed={4} className="h-6 rounded-md" />
-                    )}
-                </DrawerHeader>
-
-                <DrawerBody>
-                    {drawerState.imageUri ? (
-                        <Center>
-                            <Pressable onPress={handleSetXaiHeatmapShown}>
-                                <Image
-                                    size="2xl"
-                                    className="rounded-md min-w-full"
-                                    alt="classification-image"
-                                    source={{
-                                        uri: isXaiHeatmapShown && drawerState.xaiHeatmapUri ? drawerState.xaiHeatmapUri : drawerState.imageUri,
-                                    }}
-                                />
-                                {!isPredictionDone && (
-                                    <LottieView
-                                        style={styles.animation}
-                                        source={require("@/assets/animations/scan-animation.json")}
-                                        autoPlay
-                                        loop
-                                    />
-                                )}
-                                {drawerState.xaiHeatmapUri && (
-                                    <Text className="mt-2 opacity-50 text-center">
-                                        {isXaiHeatmapShown ? "Tap to view original Image" : "Tap to view XAI Heatmap"}
-                                    </Text>
-                                )}
-                            </Pressable>
-
-                            {RenderAiPrompts({ drawerState, setAiResponse })}
-
                             <Box className="mt-4 w-full rounded-md border-gray-300 text-white">
                                 <Markdown style={{ body: { fontSize: 16, color: "white" } }}>
-                                    {aiResponse.trim()}
+                                    {aiSession.response.trim()}
                                 </Markdown>
                             </Box>
-                        </Center>
-                    ) : (
-                        <Skeleton variant="rounded" className="h-full w-full" />
-                    )}
-                </DrawerBody>
 
-                {drawerState.confidence && drawerState.classification && drawerState.imageUri && (
-                    <DrawerFooter>
-                        {renderSaveResultComponent(drawerState.saveResultCallback, drawerState.isResultSaved)}
-                    </DrawerFooter>
+                            {/* AI Response Trigger */}
+                            {RenderAiPrompts({
+                                drawerState,
+                                setAiSession: (aiSession: AiSession) => {
+                                    setAiSession({ prompt: aiSession.prompt, response: "" });
+                                    setIsAiPageShown(true);
+                                    setAiSession(aiSession);
+                                }
+                            })}
+                        </DrawerBody>
+                    </>
+                ) : (
+                    <>
+                        <DrawerHeader>
+                            {hasResults ? (
+                                <VStack>
+                                    <Heading size="xl" className="text-left">
+                                        {drawerState.classification}
+                                    </Heading>
+                                    <Text className="text-typography-400">
+                                        {drawerState.confidence}% {renderConfidenceRemark(drawerState.confidence ?? 0)} Confidence
+                                    </Text>
+                                </VStack>
+                            ) : (
+                                <SkeletonText _lines={2} speed={4} className="h-6 rounded-md" />
+                            )}
+                        </DrawerHeader>
+
+                        <DrawerBody>
+                            {!drawerState.imageUri ? (
+                                <Skeleton variant="rounded" className="h-full w-full" />
+                            ) : (
+                                <Center>
+                                    <Pressable onPress={handleSetXaiHeatmapShown}>
+                                        <Image
+                                            size="2xl"
+                                            className="rounded-md min-w-full"
+                                            alt="classification-image"
+                                            source={{ uri: isXaiHeatmapShown && drawerState.xaiHeatmapUri ? drawerState.xaiHeatmapUri : drawerState.imageUri }}
+                                        />
+                                        {drawerState.xaiHeatmapUri && (
+                                            <Text className="mt-2 opacity-50 text-center">
+                                                {isXaiHeatmapShown ? "Tap to view original Image" : "Tap to view XAI Heatmap"}
+                                            </Text>
+                                        )}
+                                    </Pressable>
+
+                                    {!isPredictionDone && (
+                                        <LottieView
+                                            style={styles.animation}
+                                            source={require("@/assets/animations/scan-animation.json")}
+                                            autoPlay
+                                            loop
+                                        />
+                                    )}
+
+                                    {/* AI Response Trigger */}
+                                    {RenderAiPrompts({
+                                        drawerState,
+                                        setAiSession: (aiSession: AiSession) => {
+                                            setIsAiPageShown(true);
+                                            setAiSession(aiSession);
+                                        }
+                                    })}
+                                </Center>
+                            )}
+                        </DrawerBody>
+
+                        {canSaveResult && (
+                            <DrawerFooter>
+                                {renderSaveResultComponent(drawerState.saveResultCallback, drawerState.isResultSaved)}
+                            </DrawerFooter>
+                        )}
+                    </>
                 )}
             </DrawerContent>
-        </Drawer>
+        </Drawer >
     );
 };
 
